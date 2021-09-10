1ad20c35303e235998e1ca29b63fb4c66a1445b8
choi <cordial14@hanmail.net>
2021-09-10 14:15:59 +0900

    깃허브 테스트

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,18 +14,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Hello world!';
+// Route::get('/', function () {
+//     return 'Hello, world!';
+// });
+
+// Route::get('/', [WelcomeController::class,'index']);
+
+Route::get('users/{id}/friends', function ($id) {
+    return $id;
 });
 
-Route::get('about',function(){
-    return view('about');
+// Route::get('members/{id}', [MemberController::class,'show'])->name('members.show');
+
+Route::prefix('dashboard')->group(function () {
+
+    Route::get('/', function () {
+        return '대쉬보드 인덱스';
+    });
+
+    Route::get('users', function () {
+        return '대쉬보드 유저스 처리 로직';
+    });
+    
 });
 
-Route::get('products', function(){
-    return view('products');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
-Route::get('services',function(){
-    return view('services');
-});
+// Route::view('/', 'welcome',['User'=>'Michel']);
+
+Route::get('/', [TaskController::class, 'index']);
